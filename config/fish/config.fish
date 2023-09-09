19096597708e3fbc4fe445e2c071cd53bfe3ee35
HrideshG88 <37382537+HrideshG88@users.noreply.github.com>
2023-09-09 11:01:03 +0530

    Latest configs

---
+++ config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    #DECORATIONS
+    #neofetch
+   
+    #RUN
+    source ~/.config/fzf/config
+
+    #VARIABLES
+    export _JAVA_AWT_WM_NONREPARENTING=1
+    
+    #ALIASES
+    alias l="ls"
+    alias sclear="clear"
+    alias portal="python3 Portal_StillAlive_Python/still_alive_credit.py"
+    alias wgoget="wget -q --show-progress -r -nH --cut-dirs=2 --no-parent --reject='index.html*'"
+end
+#starship init fish | source
+
---
+++ config/nvim/lua/deadeye/packer.lua
@@ -0,0 +1,71 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+}
+  use {
+	 'nvim-treesitter/nvim-treesitter', {run =  ':TSUpdate'}
+}
+
+  use {
+	 'nvim-treesitter/playground'
+}
+  use {
+	  "theprimeagen/harpoon"
+}
+
+  use {
+	 'mbbill/undotree'
+}
+  use {
+	 'tpope/vim-fugitive'
+}
+  use {
+	 'lervag/vimtex'
+}
+
+use {
+	'AlphaTechnolog/pywal.nvim', as = 'pywal'
+}
+use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+}
+use {
+
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {'williamboman/mason.nvim'},           -- Optional
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
+use({
+  "epwalsh/obsidian.nvim",
+  requires = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+ },
+  config = function()
+   require("obsidian").setup({
+      dir = "~/hack/knowledgebase",
+    })
+  end,
+})
+end)
